03274e96a8a7712483a02c1bbc7394ddaa1c96bf
Nguyen Mai <nguyenmai27019@gmail.com>
2021-11-14 22:35:41 +0700

    Add

--- PlantDiseases/Controllers/DiseasesListViewController.swift
+++ PlantDiseases/Controllers/DiseasesListViewController.swift
@@ -14,6 +14,7 @@ class DiseasesListViewController: UIViewController {
     @IBOutlet weak var table: UITableView!
     
     let names = ["Peper Bell Baterial Spot", "Potato Early Blight", "Potato Late Blight", "Tomato Bacterial Spot", "Tomato Early Blight", "Tomato Late Blight", "Tomato Leaf Mold", "Tomato Target Spot"]
+    var filteredData: [String]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,25 +22,29 @@ class DiseasesListViewController: UIViewController {
         titleText.text = "Search".localized()
         searchBar.searchBarStyle = .minimal
         searchBar.placeholder = "Search here...".localized()
+        
+        filteredData = names
+        
         configTable()
     }
     
     func configTable() {
         table.delegate = self
         table.dataSource = self
+        searchBar.delegate = self
     }
 }
 
 extension DiseasesListViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return filteredData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! ListViewCell
-        cell.img.image = UIImage(named: names[indexPath.row])
-        cell.diseaseName.text = names[indexPath.row]
+        cell.img.image = UIImage(named: filteredData[indexPath.row])
+        cell.diseaseName.text = filteredData[indexPath.row]
         return cell
     }
     
@@ -49,9 +54,28 @@ extension DiseasesListViewController: UITableViewDelegate, UITableViewDataSource
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let vc = storyboard?.instantiateViewController(withIdentifier: "DetailDiseaseViewController") as? DetailDiseaseViewController {
-            vc.img = UIImage(named: names[indexPath.row])!
-            vc.lbl = names[indexPath.row]
+            vc.img = UIImage(named: filteredData[indexPath.row])!
+            vc.lbl = filteredData[indexPath.row]
             self.navigationController?.pushViewController(vc, animated: true)
         }
+    }
+}
+
+extension DiseasesListViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredData = []
+        
+        if searchText == "" {
+            filteredData = names
+        }
+        else {
+            for name in names {
+                if name.lowercased().contains(searchText.lowercased()) {
+                    filteredData.append(name)
+                }
+            }
+            
+        }
+        self.table.reloadData()
     }
 }
